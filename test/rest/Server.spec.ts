60ec849ed7cec8d5599007fe338d629509d2389b
Janice Chan <chanice@students.cs.ubc.ca>
2024-11-17 20:26:52 -0800

    removed double try catch and added smaller file than pairs

--- test/rest/Server.spec.ts
+++ test/rest/Server.spec.ts
@@ -1,7 +1,7 @@
 import { expect } from "chai";
 import request, { Response } from "supertest";
 import { StatusCodes } from "http-status-codes";
-import Log from "@ubccpsc310/folder-test/build/Log";
+// import Log from "@ubccpsc310/folder-test/build/Log";
 import Server from "../../src/rest/Server";
 import { clearDisk, getContentFromArchives } from "../TestUtil";
 
@@ -9,7 +9,7 @@ describe("Facade C3", function () {
 	let server: Server;
 	const SERVER_URL = "http://localhost:4321";
 	const ENDPOINT_URL = "/dataset/sections/sections";
-	const ZIP_FILE_DATA = getContentFromArchives("pair.zip");
+	const ZIP_FILE_DATA = getContentFromArchives("small.zip");
 
 	before(async function () {
 		const port = 4321;
@@ -28,7 +28,7 @@ describe("Facade C3", function () {
 
 	afterEach(async function () {
 		// might want to add some process logging here to keep track of what is going on
-		await clearDisk();
+		// await clearDisk();
 	});
 
 	// it("PUT test for courses dataset", function () {
@@ -56,115 +56,95 @@ describe("Facade C3", function () {
 	// });
 
 	it("PUT test success", function () {
-		try {
-			request(SERVER_URL)
-				.put(ENDPOINT_URL)
-				.send(ZIP_FILE_DATA)
-				.set("Content-Type", "application/x-zip-compressed")
-				.then(function (res: Response) {
-					expect(res.status).to.be.equal(StatusCodes.OK);
-				})
-				.catch(function () {
-					expect.fail();
-				});
-		} catch (err) {
-			Log.error(err);
-		}
+		request(SERVER_URL)
+			.put(ENDPOINT_URL)
+			.send(ZIP_FILE_DATA)
+			.set("Content-Type", "application/x-zip-compressed")
+			.then(function (res: Response) {
+				expect(res.status).to.be.equal(StatusCodes.OK);
+			})
+			.catch(function () {
+				expect.fail();
+			});
 	});
 
 	it("PUT test fail", function () {
-		try {
-			request(SERVER_URL)
-				.put("/dataset/invalid_sections/invalid_sections")
-				.send(ZIP_FILE_DATA)
-				.set("Content-Type", "application/x-zip-compressed")
-				.then(function (res: Response) {
-					expect(res.status).to.be.equal(StatusCodes.BAD_REQUEST);
-				})
-				.catch(function () {
-					expect.fail();
-				});
-		} catch (err) {
-			Log.error(err);
-		}
+		request(SERVER_URL)
+			.put("/dataset/invalid_sections/invalid_sections")
+			.send(ZIP_FILE_DATA)
+			.set("Content-Type", "application/x-zip-compressed")
+			.then(function (res: Response) {
+				expect(res.status).to.be.equal(StatusCodes.BAD_REQUEST);
+			})
+			.catch(function () {
+				expect.fail();
+			});
 	});
 
 	it("DELETE test success", function () {
-		try {
-			request(SERVER_URL)
-				.put(ENDPOINT_URL)
-				.send(ZIP_FILE_DATA)
-				.set("Content-Type", "application/x-zip-compressed")
-				.then(function (res: Response) {
-					expect(res.status).to.be.equal(StatusCodes.OK);
-				})
-				.catch(function () {
-					expect.fail();
-				});
+		request(SERVER_URL)
+			.put(ENDPOINT_URL)
+			.send(ZIP_FILE_DATA)
+			.set("Content-Type", "application/x-zip-compressed")
+			.then(function (res: Response) {
+				expect(res.status).to.be.equal(StatusCodes.OK);
+			})
+			.catch(function () {
+				expect.fail();
+			});
 
-			request(SERVER_URL)
-				.get("/datasets/sections")
-				.then(function (res: Response) {
-					expect(res.status).to.be.equal(StatusCodes.OK);
-				})
-				.catch(function () {
-					expect.fail();
-				});
-		} catch (err) {
-			Log.error(err);
-		}
+		request(SERVER_URL)
+			.get("/datasets/sections")
+			.then(function (res: Response) {
+				expect(res.status).to.be.equal(StatusCodes.OK);
+			})
+			.catch(function () {
+				expect.fail();
+			});
 	});
 
 	it("DELETE test fail with not found error", function () {
-		try {
-			request(SERVER_URL)
-				.put(ENDPOINT_URL)
-				.send(ZIP_FILE_DATA)
-				.set("Content-Type", "application/x-zip-compressed")
-				.then(function (res: Response) {
-					expect(res.status).to.be.equal(StatusCodes.OK);
-				})
-				.catch(function () {
-					expect.fail();
-				});
+		request(SERVER_URL)
+			.put(ENDPOINT_URL)
+			.send(ZIP_FILE_DATA)
+			.set("Content-Type", "application/x-zip-compressed")
+			.then(function (res: Response) {
+				expect(res.status).to.be.equal(StatusCodes.OK);
+			})
+			.catch(function () {
+				expect.fail();
+			});
 
-			request(SERVER_URL)
-				.get("/datasets/dne")
-				.then(function (res: Response) {
-					expect(res.status).to.be.equal(StatusCodes.NOT_FOUND);
-				})
-				.catch(function () {
-					expect.fail();
-				});
-		} catch (err) {
-			Log.error(err);
-		}
+		request(SERVER_URL)
+			.get("/datasets/dne")
+			.then(function (res: Response) {
+				expect(res.status).to.be.equal(StatusCodes.NOT_FOUND);
+			})
+			.catch(function () {
+				expect.fail();
+			});
 	});
 
 	it("DELETE test fail with ingisht error", function () {
-		try {
-			request(SERVER_URL)
-				.put(ENDPOINT_URL)
-				.send(ZIP_FILE_DATA)
-				.set("Content-Type", "application/x-zip-compressed")
-				.then(function (res: Response) {
-					expect(res.status).to.be.equal(StatusCodes.OK);
-				})
-				.catch(function () {
-					expect.fail();
-				});
+		request(SERVER_URL)
+			.put(ENDPOINT_URL)
+			.send(ZIP_FILE_DATA)
+			.set("Content-Type", "application/x-zip-compressed")
+			.then(function (res: Response) {
+				expect(res.status).to.be.equal(StatusCodes.OK);
+			})
+			.catch(function () {
+				expect.fail();
+			});
 
-			request(SERVER_URL)
-				.get("/datasets/bad_req")
-				.then(function (res: Response) {
-					expect(res.status).to.be.equal(StatusCodes.BAD_REQUEST);
-				})
-				.catch(function () {
-					expect.fail();
-				});
-		} catch (err) {
-			Log.error(err);
-		}
+		request(SERVER_URL)
+			.get("/datasets/bad_req")
+			.then(function (res: Response) {
+				expect(res.status).to.be.equal(StatusCodes.BAD_REQUEST);
+			})
+			.catch(function () {
+				expect.fail();
+			});
 	});
 
 	it("POST test success", function () {
@@ -180,31 +160,27 @@ describe("Facade C3", function () {
 			},
 		});
 
-		try {
-			request(SERVER_URL)
-				.put(ENDPOINT_URL)
-				.send(ZIP_FILE_DATA)
-				.set("Content-Type", "application/x-zip-compressed")
-				.then(function (res: Response) {
-					expect(res.status).to.be.equal(StatusCodes.OK);
-				})
-				.catch(function () {
-					expect.fail();
-				});
+		request(SERVER_URL)
+			.put(ENDPOINT_URL)
+			.send(ZIP_FILE_DATA)
+			.set("Content-Type", "application/x-zip-compressed")
+			.then(function (res: Response) {
+				expect(res.status).to.be.equal(StatusCodes.OK);
+			})
+			.catch(function () {
+				expect.fail();
+			});
 
-			request(SERVER_URL)
-				.post("/query")
-				.send(query)
-				.set("Content-Type", "application/json")
-				.then(function (res: Response) {
-					expect(res.status).to.be.equal(StatusCodes.OK);
-				})
-				.catch(function () {
-					expect.fail();
-				});
-		} catch (err) {
-			Log.error(err);
-		}
+		request(SERVER_URL)
+			.post("/query")
+			.send(query)
+			.set("Content-Type", "application/json")
+			.then(function (res: Response) {
+				expect(res.status).to.be.equal(StatusCodes.OK);
+			})
+			.catch(function () {
+				expect.fail();
+			});
 	});
 
 	it("POST test fail", function () {
@@ -212,56 +188,48 @@ describe("Facade C3", function () {
 			OPTIONS: {},
 		});
 
-		try {
-			request(SERVER_URL)
-				.put(ENDPOINT_URL)
-				.send(ZIP_FILE_DATA)
-				.set("Content-Type", "application/x-zip-compressed")
-				.then(function (res: Response) {
-					expect(res.status).to.be.equal(StatusCodes.OK);
-				})
-				.catch(function () {
-					expect.fail();
-				});
+		request(SERVER_URL)
+			.put(ENDPOINT_URL)
+			.send(ZIP_FILE_DATA)
+			.set("Content-Type", "application/x-zip-compressed")
+			.then(function (res: Response) {
+				expect(res.status).to.be.equal(StatusCodes.OK);
+			})
+			.catch(function () {
+				expect.fail();
+			});
 
-			request(SERVER_URL)
-				.post("/query")
-				.send(query)
-				.set("Content-Type", "application/json")
-				.then(function (res: Response) {
-					expect(res.status).to.be.equal(StatusCodes.BAD_REQUEST);
-				})
-				.catch(function () {
-					expect.fail();
-				});
-		} catch (err) {
-			Log.error(err);
-		}
+		request(SERVER_URL)
+			.post("/query")
+			.send(query)
+			.set("Content-Type", "application/json")
+			.then(function (res: Response) {
+				expect(res.status).to.be.equal(StatusCodes.BAD_REQUEST);
+			})
+			.catch(function () {
+				expect.fail();
+			});
 	});
 
 	it("GET test success", function () {
-		try {
-			request(SERVER_URL)
-				.put(ENDPOINT_URL)
-				.send(ZIP_FILE_DATA)
-				.set("Content-Type", "application/x-zip-compressed")
-				.then(function (res: Response) {
-					expect(res.status).to.be.equal(StatusCodes.OK);
-				})
-				.catch(function () {
-					expect.fail();
-				});
+		request(SERVER_URL)
+			.put(ENDPOINT_URL)
+			.send(ZIP_FILE_DATA)
+			.set("Content-Type", "application/x-zip-compressed")
+			.then(function (res: Response) {
+				expect(res.status).to.be.equal(StatusCodes.OK);
+			})
+			.catch(function () {
+				expect.fail();
+			});
 
-			request(SERVER_URL)
-				.get("/datasets")
-				.then(function (res: Response) {
-					expect(res.status).to.be.equal(StatusCodes.OK);
-				})
-				.catch(function () {
-					expect.fail();
-				});
-		} catch (err) {
-			Log.error(err);
-		}
+		request(SERVER_URL)
+			.get("/datasets")
+			.then(function (res: Response) {
+				expect(res.status).to.be.equal(StatusCodes.OK);
+			})
+			.catch(function () {
+				expect.fail();
+			});
 	});
 });
